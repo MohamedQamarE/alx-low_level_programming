45ccf56c9b297ce15fdfae0ad2a1ca5ed47a90ad
MohamedQamarE <mohamedqamar591@gmail.com>
2023-07-14 04:50:48 +0300

    remove space

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,10 @@
 /**
   * main - Entry Point
   *
-  * Return: Alwyas 1 (success)
-  *
+  * Return: Always 1 (success)
   */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
